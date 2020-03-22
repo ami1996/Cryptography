117de2c55a06046886be767689f7dbce31217089
ami1996 <61156569+ami1996@users.noreply.github.com>
2020-03-23 01:16:54 +0530

    Add files via upload

---
+++ columnar.py
@@ -0,0 +1,44 @@
+file = open('text.txt','r')
+message = file.read()
+print("\nThe message is:",message)
+
+def encryption(message):
+	key = list(map(int,input("\nenter the key: ").split()))
+
+	for c in range(-(len(message)%len(key))%len(key)):
+		message += 'x'
+
+	A = [[] for i in range(len(key))]
+	for i in range(len(message)):
+		A[i%len(key)].append(message[i])
+
+	d = {}
+	for i in range(len(key)):
+		d[key[i]] = A[i]
+
+	file = open('text.txt','w')
+	for i in range(len(key)):
+		for j in d[i]:
+			file.write(j)
+
+def decryption(message):
+	key = list(map(int,input("\nenter the key: ").split()))
+
+	A = [[] for i in range(len(key))]
+	for i in range(len(key)):
+		A[i] = message[key[i]*(len(message)//4):(key[i]+1)*(len(message)//4)]
+	A = ''.join(A)
+
+	file = open('text.txt','w')
+	for i in range(len(message)//len(key)):
+		for j in range(i,len(message),len(message)//len(key)):
+			file.write(A[j])
+
+print("\n1.encryption\n2.decryption")
+i = int(input("\nenter your choice: "))
+if(i == 1):
+	encryption(message);
+elif(i==2):
+	decryption(message);
+else:
+	print("wrong choice")
---
+++ rail.py
@@ -0,0 +1,33 @@
+file = open('text.txt','r')
+message = file.read()
+print("\nThe message is:",message)
+
+def encryption(text):
+	rails = int(input("number of rails: "))
+	m = (rails - 1) * 2
+	out = ''
+	for i in range(rails):
+		if i % (rails - 1) == 0:
+			# outer rail
+			out += text[i::m]
+		else:
+			#inner rails
+			char_pairs = zip(text[i::m], list(text[m-i::m]) + [''])
+			out += ''.join(map(''.join, char_pairs))
+	file = open('text.txt','w')
+	file.write(out)
+
+def decryption(t):
+	r = int(input("number of rails: "))
+
+	c = ''.join(t[dict((b,a)for a,b in enumerate(i+j for k in range(r)for i in range(r-1,len(t)+r,2*r-2)for j in[k-r+1,r+~k][:1+(r-1>k>0)]if i+j<len(t)))[m]]for m in range(len(t)))
+	print(c)
+
+print("\n1.encryption\n2.decryption")
+i = int(input("\nenter your choice: "))
+if(i == 1):
+	encryption(message);
+elif(i==2):
+	decryption(message);
+else:
+	print("wrong choice")
